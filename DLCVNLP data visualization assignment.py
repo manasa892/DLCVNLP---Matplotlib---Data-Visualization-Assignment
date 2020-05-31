0b1751b6a49e1a8bcf2cf488e2fe1ccc4ce841f6
manasa892 <64002205+manasa892@users.noreply.github.com>
2020-05-31 18:27:39 +0530

    Add files via upload

---
+++ DLCVNLP data visualization assignment.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+
+#Converting 4 dimnesional irisdata into 3 dimensional using PCA
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Reading the dataset
+dataset = pd.read_csv("iris.csv", header = None, names = ['sepal length', 'sepal width', 'petal length', 'petal width','class'])
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,4].values
+
+#label encoding the Target values
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+#Reducing the dimensions for 3D
+from sklearn.decomposition import PCA
+pca = PCA(n_components=3)
+X = pca.fit_transform(X)
+
+
+#Visualising the 3D chart
+from mpl_toolkits.mplot3d import Axes3D
+
+
+fig = plt.figure(1, figsize=(4, 3))
+plt.clf()
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+plt.cla()
+
+# Reorder the labels to have colors matching the cluster results
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.spectral, edgecolor='k')
+plt.show()
